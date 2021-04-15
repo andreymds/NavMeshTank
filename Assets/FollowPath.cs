2374feeea04a337e12e3c38abf051b4e54217cde
andreymds <andrey_mds@hotmail.com>
2021-04-15 01:32:25 -0300

    NavMesh

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    /*  Variáveis do WayPoint
+    Transform goal;
+    public float speed= 5.0f;
+    public float accuracy= 1.0f;
+    public float rotSpeed= 0.4f;
+    */
+
+    public GameObject wpManager;
+    GameObject[] wps;
+    UnityEngine.AI.NavMeshAgent agent;
+    
+    /* Variáveis do WayPoint
+    GameObject currentNode;
+    int currentWP= 0;
+    Graph g;
+    */
+    void Start()
+    {
+        wps = wpManager.GetComponent<WpManager>().waypoints;
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>(); //acesso ao componente do navMesh
+        //g = wpManager.GetComponent<WpManager>().graph; 
+        //currentNode = wps[0]; //o nó que o tank se encontra no meio da matriz
+    }
+    public void GoToHeli() 
+    {
+        agent.SetDestination(wps[1].transform.position); //define o waypont de destino
+        //g.AStar(currentNode, wps[1]); //algoritmo A*
+        //currentWP = 0; //define como destino final
+    }
+    public void GoToRuin() 
+    {
+        agent.SetDestination(wps[5].transform.position);
+        //g.AStar(currentNode, wps[5]); 
+        //currentWP = 0; 
+    }
+    public void GoToIndustry()
+    {
+        agent.SetDestination(wps[4].transform.position);
+        //g.AStar(currentNode, wps[4]);
+        //currentWP = 0;
+    }
+    void LateUpdate()
+    {
+        /* CÓDIGO USADO NO WAYPOINT
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength()) //envia o tamanho do caminho a ser percorrido
+            return;
+        //o nó atual
+        currentNode= g.getPathPoint(currentWP);
+
+        //se movimenta para o próximo nó
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy) 
+        { 
+            currentWP++;            
+        }
+        
+        if (currentWP < g.getPathLength()) 
+        { 
+            goal = g.getPathPoint(currentWP).transform; //define o destino 
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z); 
+            Vector3 direction = lookAtGoal - this.transform.position; //define o vetor entre a posição atual e o destino
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, //rotação suave
+                Quaternion.LookRotation(direction), 
+                Time.deltaTime * rotSpeed); 
+        }
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+        */
+    }
+}
+
---
+++ Assets/WpManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] //distribuição de informações que transmitem dados para diferentes tipos de classes
+
+//constroem as conexões entre os pontos
+public struct Link{  
+    public enum direction { UNI, BI } //define se é unidirecional ou bidirecional
+    public GameObject node1; 
+    public GameObject node2; 
+    public direction dir;
+}
+public class WpManager : MonoBehaviour
+{
+    public GameObject[] waypoints; 
+    public Link[] links; 
+    public Graph graph = new Graph(); //cria objeto Grafo
+
+    void Start()
+    {
+        if (waypoints.Length > 0) //condição de existência do Grafo
+        { 
+            foreach (GameObject wp in waypoints)
+            { 
+                graph.AddNode(wp); 
+            } 
+            foreach (Link l in links) //relacionando o array com os links criados
+            { 
+                graph.AddEdge(l.node1, l.node2); //adiciona uma aresta entre os nós 
+                
+                //se caso for bidirecional adiciona uma aresta entre os mesmos nós na direção contrária
+                if (l.dir == Link.direction.BI)                     
+                    graph.AddEdge(l.node2, l.node1); 
+            } 
+        }
+    }
+        
+    void Update()
+    {
+        graph.debugDraw(); // traça visualmente as arestas
+    }
+}
